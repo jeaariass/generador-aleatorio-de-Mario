a805871ff66c33104eb53f62c25afff5e377ff65
Jesus Arias <jeaariass@udistrital.edu.co>
2026-09-22 17:02:38 -0500

    actividad1

--- actividades/actividad1/actividad1.m
+++ actividades/actividad1/actividad1.m
@@ -1,22 +1,4 @@
 function [posbloques2,postubos2] = actividad1(posbloques,postubos)
-% ACTIVIDAD1 Primera actividad (con los pies en la tierra).
-%
-% [posbloques2,postubos2] = ACTIVIDAD1(posbloques,postubos)
-%
-% Entradas:
-%   posbloques : matriz 12x2 con las dos ultimas columnas de la matriz
-%                de bloques del escenario.
-%   postubos   : vector 6x1 con la ultima columna de la matriz de tubos.
-%
-% Salidas:
-%   posbloques2 : matriz 12x2. Las filas 1 a 11 quedan intactas (iguales
-%                 a las de entrada). La fila 12 (piso) se genera de
-%                 forma aleatoria y solo puede tomar los valores 1
-%                 (negro/hueco) o 2 (baldosa).
-%   postubos2   : igual a postubos, sin ninguna alteracion.
-%
-% Las probabilidades de la fila 12 se fijan aqui abajo; ajustelas para
-% controlar la proporcion de huecos frente a piso solido (ver Tarea 6).
 
     if ~isequal(size(posbloques),[12,2])
         error('actividad1:tamano','posbloques debe ser 12x2.');
@@ -27,7 +9,7 @@ function [posbloques2,postubos2] = actividad1(posbloques,postubos)
 
     % Valores posibles para la fila 12 y su probabilidad.
     x = [1,2];               % 1 = negro (hueco), 2 = baldosa (piso)
-    p = [0.12,0.88];         % ajustar segun Tarea 6
+    p = [0.12,0.88];         
 
     posbloques2 = posbloques;
     posbloques2(12,:) = va(x,p,1,2);
